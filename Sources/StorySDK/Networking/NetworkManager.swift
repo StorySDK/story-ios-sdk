20b2cfd2558945551d89d4c955d94458f3aa7d60
Aleksei Cherepanov <ftp27host@gmail.com>
2022-11-14 13:36:14 +0300

    RFC3339 DateFormatter fixed

--- Sources/StorySDK/Networking/NetworkManager.swift
+++ Sources/StorySDK/Networking/NetworkManager.swift
@@ -189,6 +189,8 @@ private extension DateFormatter {
     static let rfc3339: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
     }()
 }

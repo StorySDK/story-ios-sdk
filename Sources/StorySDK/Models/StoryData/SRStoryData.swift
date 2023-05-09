fac37d7c1de880b61eb255faaa760a6458bd02e8
sjackk <sjack0684@gmail.com>
2023-05-09 18:58:43 +0300

    Only groups & onboarding are displayed, separate story displaying condition improved

--- Sources/StorySDK/Models/StoryData/SRStoryData.swift
+++ Sources/StorySDK/Models/StoryData/SRStoryData.swift
@@ -12,9 +12,11 @@ public struct SRStoryData: Decodable {
     public var background: BRColor?
     public var status: SRStoryStatus
     public var widgets: [SRWidget]
+    public var startTime: TimeInterval
+    public var endTime: TimeInterval
     
     enum CodingKeys: String, CodingKey {
-        case background, status, widgets
+        case background, status, widgets, startTime, endTime
     }
     
     public init(from decoder: Decoder) throws {
@@ -22,6 +24,17 @@ public struct SRStoryData: Decodable {
         background = try? container.decode(BRColor.self, forKey: .background)
         status = (try? container.decode(SRStoryStatus.self, forKey: .status)) ?? .invalid
         widgets = try container.decode([SRWidget].self, forKey: .widgets)
+        
+        let innerStartTime = try? container.decode(String.self, forKey: .startTime)
+        let innerEndTime = try? container.decode(String.self, forKey: .endTime)
+        
+        startTime = TimeInterval(innerStartTime ?? "") ?? 0
+        endTime = TimeInterval(innerEndTime ?? "") ?? TimeInterval.infinity
+    }
+    
+    public func readyToShow() -> Bool {
+        let timestamp = TimeInterval(Date().timeIntervalSince1970 * 1000)
+        return (startTime < timestamp) && (timestamp < endTime)
     }
 }
 
